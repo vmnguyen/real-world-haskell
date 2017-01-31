d06851b2f6e52c33821ea29ef98e5c8da78ccb2e
vmnguyen <51302307@hcmut.edu.vn>
2017-01-31 22:06:25 +0700

    Add chapter 3 and material

---
+++ ch03/BookStore.hs
@@ -0,0 +1,5 @@
+-- file: ch03/BookStore.hs
+data BookInfo = Book Int String [String]
+                deriving (Show)
+
+
---
+++ ch03/myInfo.hs
@@ -0,0 +1,2 @@
+--file ch03/myInfo.hs
+myInfo = Book 11111 "ABC D" ["12 34", "56 78"]
